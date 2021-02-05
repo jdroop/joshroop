765e2a49c2533b1da556a67d2662d0b20660d1c2
Josh Roop <joshroop@Joshs-MacBook-Air.local>
2021-02-05 15:18:33 -0600

    adding installer files and nba-app base

---
+++ installer/installer.py
@@ -0,0 +1,8 @@
+import sys, os, json
+import requests
+import shutil
+from zipfile import ZipFile
+from functools import partial
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
